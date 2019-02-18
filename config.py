8d1b288707dd59a560f6c749956a7fe273bda480
James Musyoka <jamesmu475@gmail.com>
2019-02-18 10:19:33 +0300

    updated

--- config.py
+++ config.py
@@ -1,30 +1,18 @@
 import os
 
 class Config:
- """
- General configuration parent class
- """
-BASE_URL = 'http://quotes.stormconsultancy.co.uk/{}.json'
+ 
+ BASE_URL = 'http://quotes.stormconsultancy.co.uk/{}.json'
  SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 class ProdConfig(Config):
- """
- Production configuration child class
- Args:
-   Config: The parent configuration class with General
-   configuration settings
- """
+ 
  pass
 
 
 class DevConfig(Config):
- """
- Development configuration child class
- Args:
-   Config: The parent configuration class with General
-   configuration settings
- """
+ 
 
  DEBUG = True
 
