45c9d90b229a2a265dc001a00c3b4610c01ecc2c
James Musyoka <jamesmu475@gmail.com>
2019-02-19 02:10:37 +0300

    Api key created

--- config.py
+++ config.py
@@ -2,19 +2,31 @@ import os
 
 class Config:
  
- BASE_URL = 'http://quotes.stormconsultancy.co.uk/{}.json'
- SECRET_KEY = os.environ.get('SECRET_KEY')
-
+ 
+ SECRET_KEY = os.environ.get('SECRET_KEY') or 'you will-never-guess '
+ SQLALCHEMY_TRACK_MODIFICATIONS = False
+ SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:37472377@localhost/flaskblog'
+ UPLOADED_PHOTOS_DEST ='app/static/photos'
+ MAIL_SERVER = 'smtp.googlemail.com'
+ MAIL_PORT = 587
+ MAIL_USE_TLS = True
+ MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+ MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+ SIMPLEMDE_JS_IIFE = True
+ SIMPLEMDE_USE_CDN = True
 
 class ProdConfig(Config):
- 
- pass
+  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:37472377@localhost/flaskblog'
+  pass
 
+
+class TestConfig(Config):
+  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:37472377@localhost/flaskblog_test'
 
 class DevConfig(Config):
- 
+  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:37472377@localhost/flaskblog'
 
- DEBUG = True
+  DEBUG = True
 
 
 config_options = {
