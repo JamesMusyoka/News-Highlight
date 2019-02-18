72dbb0f412b2ce8143af26e6fef86c6bf1d5d81e
James Musyoka <jamesmu475@gmail.com>
2019-02-19 02:15:22 +0300

    updated

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,8 +3,17 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField
 from wtforms.validators import Required
 
-class ReviewForm(FlaskForm):
+class BlogForm(FlaskForm):
 
    title = StringField('Review title',validators=[Required()])
-   review = TextAreaField('source review', validators=[Required()])
+   blog = TextAreaField('headings')
    submit = SubmitField('Submit')
+
+class UpdateProfile(FlaskForm):
+   bio = TextAreaField('Tell us about you.',validators = [Required()])
+   submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+    name = StringField('Comment title', validators = [Required()])
+    comment = TextAreaField('blog a Comment',validators = [Required()])
+    submit = SubmitField('Add Comment')
