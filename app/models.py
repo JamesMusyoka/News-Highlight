abafac0d10cf38ac6435361ee3d0cb30f5b02eb9
James Musyoka <jamesmu475@gmail.com>
2019-02-19 02:13:12 +0300

    adding a functionfor the app

--- app/models.py
+++ app/models.py
@@ -1,24 +1,110 @@
-class Quotes:
- """
- News class to define  news objects
- """
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+from datetime import datetime
 
- def __init__(self,id,name,url,category,description):
-     self.id = id
-     self.name = name
-     self.url = url
-     self.category = category
-     self.description = description
 
-class Random:
- """
- Headlines class to define news headlines objects
- """
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
 
- def __init__(self,id,title,description,url,urlToImage,publishedAt):
-     self.id = id
-     self.title = title
-     self.description = description
-     self.url = url
-     self.urlToImage = urlToImage
-     self.publishedAt = publishedAt
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_secure = db.Column(db.String(255))
+    
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+    def __repr__(self):
+        return f'User {self.name}'
+
+
+class Blog(db.Model):
+    __tablename__ = 'blogs'
+    id = db.Column(db.Integer,primary_key = True)
+    blog_title = db.Column(db.String)
+    blog_content = db.Column(db.String(1000))
+    category = db.Column(db.String(), nullable = False)
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    comments = db.relationship('Comment',backref =  'blog_id',lazy = "dynamic")
+
+
+    def save_blog(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_blogs(cls,category):
+        blogs = Blog.query.filter_by(category = category).all()
+        return blogs
+
+    @classmethod
+    def get_blog(cls,id):
+        blog = Blog.query.filter_by(id = id).first()
+
+        return blog
+
+    def __repr__(self):
+        return f'Blog {self.blog_title}, {self.category}'
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer,primary_key = True)
+    comment = db.Column(db.String(1000))
+    name = db.Column(db.String)
+    blog = db.Column(db.Integer,db.ForeignKey("blogs.id"))
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls,blog):
+        comments = Comment.query.filter_by(blog_id = blog).all()
+        return comments
+
+    @classmethod
+    def delete_comment(cls,id):
+        comment = Comment.query.filter_by(id=id).first()
+        db.session.delete(comment)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'Comment{self.comment}'
+
+
+class Subscriber(db.Model):
+    __tablename__ = 'subscribers'
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(20))
+    email = db.Column(db.String(), unique = True)
+
+
