5f1f62ffe8a548d2b7f9fd4a49d9ccea2c4ecdec
James Musyoka <jamesmu475@gmail.com>
2019-02-18 08:55:50 +0300

    Created my BASE_URL

--- config.py
+++ config.py
@@ -4,12 +4,7 @@ class Config:
  """
  General configuration parent class
  """
- NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
- NEWS_API_SECONDARY_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
- NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
- HEADLINES_KEY = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=597b0dec338d4c37a52b93e8570d3cf7'
- SOURCE_API = 'https://newsapi.org/v2/sources?apiKey=597b0dec338d4c37a52b93e8570d3cf7'
-#  ARTICLE_API
+BASE_URL = 'http://quotes.stormconsultancy.co.uk/{}.json'
  SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
